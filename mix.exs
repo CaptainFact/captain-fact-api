337d5f2ee173c2aaf3328318e476b939ed175c8f
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-05-23 16:00:21 +1100

    Increase version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CaptainFact.Mixfile do
 
   def project do
     [app: :captain_fact,
-     version: "0.0.1",
+     version: "0.2.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
