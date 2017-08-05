55b55da2c06ad52c5b969023a93042dd297e4acd
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-08-05 16:41:57 +1100

    Upgrade to elixir 1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CaptainFact.Mixfile do
   def project do
     [app: :captain_fact,
      version: "0.3.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
