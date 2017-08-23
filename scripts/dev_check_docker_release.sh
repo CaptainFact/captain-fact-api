75f6eb5cec91b5079e5e22cb28f9e7152a24e155
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-08-23 22:24:52 +1100

    New build system based on build then build release. Remove CI
    CI will now be called by parent project

--- scripts/dev_check_docker_release.sh
+++ scripts/dev_check_docker_release.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 #---------------------------------------------------------------------------------------------------
 # Start a docker release with dev params
-# Usage ./test_docker_release.sh container
+# Usage ./test_docker_release.sh
 #
 # /!\ Obviously database must be started
 #---------------------------------------------------------------------------------------------------
 
+
+CF_API_IMAGE=captain-fact-api:dev-check
+CF_API_BUILD_IMAGE=captain-fact-api-build:dev-check
+
+# Build
+docker build -t ${CF_API_BUILD_IMAGE} -f Dockerfile.build .
+BUILD_CONTAINER=$(docker run -d ${CF_API_BUILD_IMAGE})
+docker cp ${BUILD_CONTAINER}:/opt/app/captain-fact-api_release.tar ./captain-fact-api_release.tar
+docker build -t ${CF_API_IMAGE} -f Dockerfile.release .
+
+# Run server
 docker run -it \
   -p 4000:80 \
   -p 4001:443 \
@@ -24,4 +35,9 @@ docker run -it \
   -e "FRONTEND_URL=http://localhost:3333" \
   -e "CHROME_EXTENSION_ID=chrome-extension://lpdmcoikcclagelhlmibniibjilfifac" \
   --network host \
-  --rm $1 foreground
+  --rm ${CF_API_IMAGE} foreground
+
+# Cleanup
+docker stop ${BUILD_CONTAINER} && docker rm ${BUILD_CONTAINER}
+docker rmi -f ${CF_API_IMAGE} ${CF_API_BUILD_IMAGE}
+rm captain-fact-api_release.tar
