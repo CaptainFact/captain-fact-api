54c850eac3f1bba9a350163fb5c5c1772bd493ae
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-10-28 16:20:20 +1100

    Update version to 0.6.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CaptainFact.Mixfile do
   def project do
     [
       app: :captain_fact,
-      version: "0.6.1",
+      version: "0.6.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
