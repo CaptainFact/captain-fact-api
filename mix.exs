555afb97da594595419c8c59e5df6c46d46d4899
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-10-30 11:57:11 +1100

    Update limitations (bis)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CaptainFact.Mixfile do
   def project do
     [
       app: :captain_fact,
-      version: "0.6.2",
+      version: "0.6.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
