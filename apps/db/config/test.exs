0888f70e7f3e881b36386f63f29aa16b168693f8
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-01-23 14:41:25 +1100

    Test: get db hostname in env during build

--- apps/db/config/test.exs
+++ apps/db/config/test.exs
@@ -9,6 +9,7 @@ config :logger, level: :warn
 
 # Configure your database
 config :db, DB.Repo,
+  hostname: (if is_nil(System.get_env("CF_DB_HOSTNAME")), do: "localhost", else: System.get_env("CF_DB_HOSTNAME")),
   username: "postgres",
   password: "postgres",
   database: "captain_fact_test",
