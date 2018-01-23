64a2201bd7b9110dff5535b0632d18419e6f1239
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-01-23 12:37:19 +1100

    Load runtime configuration in test

--- apps/db/test/test_helper.exs
+++ apps/db/test/test_helper.exs
@@ -3,5 +3,9 @@
 ExUnit.start
 Faker.start
 
+# Load runtime configuration
+DB.RuntimeConfiguration.setup()
+DB.RuntimeConfiguration.configure()
+
 Ecto.Adapters.SQL.Sandbox.mode(DB.Repo, :manual)
 {:ok, _} = Application.ensure_all_started(:ex_machina)
