ef50a8b7de33fb70494f56c2034dcde889140e97
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-02-26 21:08:56 +1100

    Fixed comment's actions rendering in moderation
    Update existing comments to add text and source to their history with:
    
    ```
    import Ecto.Query
    
    actions = DB.Repo.all(from(
      a in DB.Schema.UserAction,
      where: a.entity == 4,
      where: a.type == 1,
      where: is_nil(a.changes)
    ))
    
    for action <- actions do
      case DB.Repo.get(DB.Schema.Comment, action.entity_id) do
        nil -> nil # Deleted comment
        comment ->
          comment = DB.Repo.preload(comment, :source)
          source_url = comment.source && comment.source.url
          changes = %{text: comment.text, source: source_url}
          DB.Repo.update(Ecto.Changeset.change(action, changes: changes))
        end
    end
    ```

--- apps/captain_fact/priv/demos/demo_fr.ex
+++ apps/captain_fact/priv/demos/demo_fr.ex
@@ -183,6 +183,7 @@ defmodule CaptainFact.DemoFr do
   end
 
   defp add_comment(users, video_id, statement_id, comment_base, reply_to_id \\ nil) do
+    context = DB.Schema.UserAction.video_debate_context(video_id)
     params =
       comment_base
       |> Map.take([:text, :approve])
@@ -191,7 +192,7 @@ defmodule CaptainFact.DemoFr do
       |> Map.put(:source, (comment_base[:source] && %{url: comment_base.source}) || nil)
 
     comment =
-      Comments.add_comment(Enum.at(users, comment_base.user_idx), "DEMO", params, comment_base[:source], fn comment ->
+      Comments.add_comment(Enum.at(users, comment_base.user_idx), context, params, comment_base[:source], fn comment ->
         comment = Repo.preload(comment, :source) |> Repo.preload(:user)
         CaptainFactWeb.Endpoint.broadcast(
           "comments:video:#{VideoHashId.encode(video_id)}", "comment_updated",
