cf3c10555c0c9a001e96e4b785b143e01bcc10fe
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-03-25 20:01:55 +1100

    Bump version to 0.8.6

--- apps/captain_fact_atom_feed/mix.exs
+++ apps/captain_fact_atom_feed/mix.exs
@@ -4,7 +4,7 @@ defmodule CaptainFactAtomFeed.Mixfile do
   def project do
     [
       app: :captain_fact_atom_feed,
-      version: "0.8.3",
+      version: "0.8.6",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
