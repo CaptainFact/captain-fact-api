37ae3893b17f56f880f75f80b76b80623c6375f7
Julien Leclercq <julien.leclercq@gmx.fr>
2018-06-12 14:18:02 +0200

    install plug and cowboy

--- apps/opengraph/mix.exs
+++ apps/opengraph/mix.exs
@@ -21,8 +21,8 @@ defmodule Opengraph.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:plug, "~> 1.5.1"},
+      {:cowboy, "~> 1.1"}
     ]
   end
 end
