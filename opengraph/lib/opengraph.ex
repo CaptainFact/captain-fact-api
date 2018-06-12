f175430bc71d7fb2d945e2311bb7ad2c54fdf92a
Julien Leclercq <julien.leclercq@gmx.fr>
2018-06-12 14:08:05 +0200

    init open graph app

---
+++ opengraph/lib/opengraph.ex
@@ -0,0 +1,18 @@
+defmodule Opengraph do
+  @moduledoc """
+  Documentation for Opengraph.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Opengraph.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
