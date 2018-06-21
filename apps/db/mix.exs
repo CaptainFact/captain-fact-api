d28cb0b25e0402fca5fb8639a05125bc046e9a41
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-06-21 16:59:15 +0200

    Bump version to 0.8.11 (and fix path for test resources)

--- apps/db/mix.exs
+++ apps/db/mix.exs
@@ -4,7 +4,7 @@ defmodule DB.Mixfile do
   def project do
     [
       app: :db,
-      version: "0.8.10",
+      version: "0.8.11",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
