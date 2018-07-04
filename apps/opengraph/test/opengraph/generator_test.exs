f4df3f660967129cb6f5ca9f18fbf5cf0ad9f2cc
Julien Leclercq <julien.leclercq@gmx.fr>
2018-07-04 11:38:32 +0200

    remove obsolete tests

--- apps/opengraph/test/opengraph/generator_test.exs
+++ apps/opengraph/test/opengraph/generator_test.exs
@@ -1,28 +1,4 @@
 defmodule Opengraph.GeneratorTest do
   use ExUnit.Case
-
-  import DB.Factory
-
-  alias Kaur.Result
-  alias Opengraph.Generator
-
   doctest Opengraph.Generator
-
-  describe "video_tags/1" do
-    setup _context do
-      [video: insert(:video)]
-    end
-
-    test "it is not currently implemented", context do
-      video = context[:video]
-
-      assert(Generator.video_tags(video)) == Result.error("Not implemented")
-    end
-
-    test "it returns {:ok, value} for a valid video", context do
-      video = context[:video]
-
-      assert {:ok, _} = Generator.video_tags(video)
-    end
-  end
 end
--- apps/opengraph/test/opengraph/router_test.exs
+++ apps/opengraph/test/opengraph/router_test.exs
@@ -14,8 +14,8 @@ defmodule Opengraph.RouterTest do
         conn(:get, "/u/#{user.username}")
         |> Router.call([])
 
-        # TODO investigate random warning
-        assert response.status == 200
+      # TODO investigate random warning
+      assert response.status == 200
     end
 
     test "returns 404 for an unknown user" do
@@ -30,17 +30,6 @@ defmodule Opengraph.RouterTest do
       assert response.status == 404
     end
 
-    test "returns valid xml" do
-      user = insert(:user)
-
-      %{resp_body: body} =
-        conn(:get, "/u/#{user.username}")
-        |> Router.call([])
-
-      assert SweetXml.parse(body)
-    end
-
-    # TODO add some more specific tests
   end
 
   describe "get /videos" do
@@ -48,12 +37,6 @@ defmodule Opengraph.RouterTest do
       response = conn(:get, "/videos", []) |> Router.call([])
 
       assert response.status == 200
-    end
-
-    test "returns valid xml" do
-      %{resp_body: body} = conn(:get, "/videos", []) |> Router.call([])
-
-      assert SweetXml.parse(body)
     end
   end
 
@@ -83,12 +66,8 @@ defmodule Opengraph.RouterTest do
       assert response.status == 404
     end
 
-    test "returns valid xml for a valid video", context do
-      %{resp_body: body} = conn(:get, "videos/#{context[:video_id]}") |> Router.call([])
-
-      assert SweetXml.parse(body)
-    end
   end
+
   describe "get /videos/:video_id/history" do
     setup _ do
       video = insert(:video)
@@ -113,12 +92,6 @@ defmodule Opengraph.RouterTest do
       response = conn(:get, "videos/#{video_id}/history") |> Router.call([])
 
       assert response.status == 404
-    end
-
-    test "returns valid xml for a valid video", context do
-      %{resp_body: body} = conn(:get, "videos/#{context[:video_id]}/history") |> Router.call([])
-
-      assert SweetXml.parse(body)
     end
   end
 end
