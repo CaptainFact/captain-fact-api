c0e7ad03fb05a07a871150667a1b9f541c5b572e
Julien Leclercq <julien.leclercq@gmx.fr>
2018-07-04 16:00:44 +0200

    text in french + format

--- apps/opengraph/lib/opengraph/generator.ex
+++ apps/opengraph/lib/opengraph/generator.ex
@@ -7,29 +7,34 @@ defmodule Opengraph.Generator do
   This module gather functions to generate an xml_builder tree based on
   """
 
-  @template_path fn () ->
+  @template_path fn ->
     # In case of a non umbrella deployment
-    if Mix.Project.umbrella? do
+    if Mix.Project.umbrella?() do
       "./apps/opengraph"
     else
       "."
     end
-    |> fn root ->
-        Path.join(root, "lib/opengraph/template.html.eex")
-    end.()
+    |> (fn root ->
+          Path.join(root, "lib/opengraph/template.html.eex")
+        end).()
   end
 
-  EEx.function_from_file :defp, :do_render, @template_path.(), [:description, :image, :title,  :url], engine: Phoenix.HTML.Engine
+  EEx.function_from_file(
+    :defp,
+    :do_render,
+    @template_path.(),
+    [:description, :image, :title, :url],
+    engine: Phoenix.HTML.Engine
+  )
 
   defp render(%{
-    description: description,
-    image: image,
-    title: title,
-    url: url
-    })
-  do
+         description: description,
+         image: image,
+         title: title,
+         url: url
+       }) do
     do_render(description, image, title, url)
-    |> Phoenix.HTML.safe_to_string
+    |> Phoenix.HTML.safe_to_string()
   end
 
   # --- User ----
@@ -39,19 +44,17 @@ defmodule Opengraph.Generator do
   """
   def render_user(user = %User{}, path) do
     encoded_url =
-    "www.captainfact.io#{path}"
-    |> URI.encode()
+      "www.captainfact.io#{path}"
+      |> URI.encode()
 
     escaped_username = Plug.HTML.html_escape(user.username)
 
-    render(
-      %{
-        title: "#{escaped_username}'s profile on Captain Fact",
-        url: encoded_url,
-        description: "discover #{escaped_username}'s profile on Captain Fact",
-        image: DB.Type.UserPicture.url({user.picture_url, user}, :thumb)
-      }
-    )
+    render(%{
+      title: "le profil de : #{escaped_username} sur Captain Fact",
+      url: encoded_url,
+      description: "Découvrez le profil de #{escaped_username} sur Captain Fact",
+      image: DB.Type.UserPicture.url({user.picture_url, user}, :thumb)
+    })
   end
 
   # ---- Videos ----
@@ -61,9 +64,10 @@ defmodule Opengraph.Generator do
   """
   def render_videos_list(path) do
     render(%{
-      title: "Every videos crowd sourced and fact checked on Captain Fact",
+      title: "Les vidéos sourcées et vérifiées sur Captain Fact",
       url: "www.captainfact.io#{path}",
-      description: "Discover the work of Captain Fact's community on diverse videos",
+      description:
+        "Découvrez diverses vidéos sourcées et vérifiées par la communauté Captain Fact",
       image: "captainfact.io/assets/img/logo.png"
     })
   end
@@ -95,5 +99,4 @@ defmodule Opengraph.Generator do
     }
     |> render
   end
-
 end
