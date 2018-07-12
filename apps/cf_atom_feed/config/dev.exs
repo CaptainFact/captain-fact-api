bbc20915018daff7a0bc12869d264c2a5b3e2f63
Julien Leclercq <julien.leclercq@gmx.fr>
2018-07-12 14:26:27 +0200

    remove useless and buggy config

--- apps/cf_atom_feed/config/dev.exs
+++ apps/cf_atom_feed/config/dev.exs
@@ -1,7 +1,4 @@
 use Mix.Config
 
-config :captain_fact_atom_feed, CaptainFactAtomFeed.Router
-
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
-
