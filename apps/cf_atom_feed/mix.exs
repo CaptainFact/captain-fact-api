0271bb5e62f325ea450d136389c8eacbb69d49cc
Julien Leclercq <julien.leclercq@gmx.fr>
2018-07-12 14:27:05 +0200

    rename application

--- apps/cf_atom_feed/mix.exs
+++ apps/cf_atom_feed/mix.exs
@@ -3,7 +3,7 @@ defmodule CaptainFactAtomFeed.Mixfile do
 
   def project do
     [
-      app: :captain_fact_atom_feed,
+      app: :cf_atom_feed,
       version: "0.8.6",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
