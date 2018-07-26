e150f8e7df9489d160ada1fa9e2dcc5c0c4d6009
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-07-26 15:37:58 +0200

    Split invitations system in a separate module

---
+++ apps/captain_fact/lib/captain_fact/accounts/invitations.ex
@@ -0,0 +1,158 @@
+defmodule CaptainFact.Accounts.Invitations do
+  @moduledoc """
+  Invitation system. Generate invites, validate them...
+
+  Can be bypassed by setting the {:captain_fact, :invitation_system} env variable
+  to false.
+  """
+
+  require Logger
+  import Ecto.Query
+
+  alias DB.Utils.TokenGenerator
+  alias DB.Schema.InvitationRequest
+  alias DB.Schema.User
+  alias DB.Repo
+
+  @doc """
+  Returns true if invitation can be safely used, false otherwise
+  """
+  def invitation_valid?(nil),
+    do: false
+
+  def invitation_valid?(invitation_token) when is_binary(invitation_token),
+    do: do_invitation_valid?(invitation_token)
+
+  def invitation_valid?(%InvitationRequest{token: token}),
+    do: do_invitation_valid?(token)
+
+  @doc """
+  Consume one invitation for given `invitation_token`. We willingly delete token
+  using `invitation_token` string because we accept having multiple invitations
+  with the same token.
+  """
+  def consume_invitation(invitation_token) do
+    case get_invitation_for_token(invitation_token) do
+      nil ->
+        {:error, nil}
+
+      invit ->
+        Repo.delete(invit)
+
+        Logger.debug(fn ->
+          "Invitation #{invit.id} for token #{invit.token} has been consumed"
+        end)
+    end
+  end
+
+  # --------
+
+  @doc """
+  Request an invitation for given email
+  """
+  def request_invitation(email, invited_by_id \\ nil, locale \\ nil)
+
+  def request_invitation(email, invited_by_id, locale)
+      when is_nil(invited_by_id) or is_integer(invited_by_id) do
+    with true <- Regex.match?(User.email_regex(), email),
+         false <- Burnex.is_burner?(email) do
+      case Repo.get_by(InvitationRequest, email: email) do
+        nil ->
+          %InvitationRequest{}
+          |> InvitationRequest.changeset(%{
+            email: email,
+            invited_by_id: invited_by_id,
+            locale: locale
+          })
+          |> Repo.insert()
+
+        %{invitation_sent: true} = invit ->
+          Repo.update(InvitationRequest.changeset_sent(invit, false))
+
+        request ->
+          {:ok, request}
+      end
+    else
+      _ -> {:error, "invalid_email"}
+    end
+  end
+
+  def request_invitation(email, %User{id: id}, locale),
+    do: request_invitation(email, id, locale)
+
+  @doc """
+  Send `nb_invites` invitations to most recently updated users
+  """
+  def send_invites(nb_invites) do
+    InvitationRequest
+    |> where([i], not is_nil(i.email))
+    |> where([i], i.invitation_sent == false)
+    |> order_by([i], i.updated_at)
+    |> preload(:invited_by)
+    |> limit(^nb_invites)
+    |> Repo.all()
+    |> Enum.each(&send_invite/1)
+  end
+
+  @default_token_length 8
+  @doc """
+  Send invite to the given email or invitation request
+  """
+  def send_invite(email) when is_binary(email) do
+    {:ok, request} = request_invitation(email)
+    send_invite(request)
+  end
+
+  def send_invite(request = %InvitationRequest{token: nil}) do
+    request
+    |> InvitationRequest.changeset_token(TokenGenerator.generate(@default_token_length))
+    |> Repo.update!()
+    |> send_invite()
+  end
+
+  def send_invite(request = %InvitationRequest{}) do
+    request
+    |> Repo.preload(:invited_by)
+    |> CaptainFactMailer.Email.invitation_to_register()
+    |> CaptainFactMailer.deliver_later()
+
+    # Email sent successfuly
+    Repo.update!(InvitationRequest.changeset_sent(request, true))
+  end
+
+  @doc """
+  Generate `number` invitations. You can specify a custom `token`
+  """
+  def generate_invites(number),
+    do: generate_invites(number, TokenGenerator.generate(@default_token_length))
+
+  def generate_invites(number, token) do
+    time = Ecto.DateTime.utc()
+
+    Repo.insert_all(
+      InvitationRequest,
+      for(_ <- 1..number, do: %{token: token, inserted_at: time, updated_at: time})
+    )
+
+    frontend_url = Application.fetch_env!(:captain_fact, :frontend_url)
+
+    Logger.info(
+      "Generated #{number} invites for token #{token}. Url: #{frontend_url}/signup?invitation_token=#{
+        token
+      }"
+    )
+  end
+
+  @doc """
+  Return an invitation for given token, or nil if token is invalid
+  """
+  def get_invitation_for_token(token) do
+    InvitationRequest
+    |> where(token: ^token)
+    |> limit(1)
+    |> Repo.one()
+  end
+
+  defp do_invitation_valid?(token),
+    do: not is_nil(get_invitation_for_token(token))
+end
--- apps/captain_fact/test/captain_fact/accounts/accounts_test.exs
+++ apps/captain_fact/test/captain_fact/accounts/accounts_test.exs
@@ -5,7 +5,7 @@ defmodule CaptainFact.AccountsTest do
   alias CaptainFact.Accounts
   alias CaptainFactJobs.{Reputation, Achievements}
 
-  alias DB.Schema.{InvitationRequest, User}
+  alias DB.Schema.User
 
   alias Kaur.Result
 
@@ -104,58 +104,6 @@ defmodule CaptainFact.AccountsTest do
         |> Repo.aggregate(:count, :token)
       assert nb_requests == 0
     end
-  end
-
-  describe "invitation_requests" do
-    test "invitation request get created with given invited_by user" do
-      email = "test@email.com"
-      user = insert(:user)
-      {:ok, req} = Accounts.request_invitation(email, user)
-      assert is_nil(req.token), "don't generate token before necessary"
-      assert user.id == req.invited_by_id
-      assert email == req.email
-    end
-
-    test "send a mail when calling send_invite/1" do
-      req = insert(:invitation_request)
-      Accounts.send_invite(req)
-      assert_delivered_email CaptainFactMailer.Email.invitation_to_register(req)
-    end
-
-    test "send mails when calling send_invites/1" do
-      Repo.delete_all(InvitationRequest)
-      nb_invites = 10
-      requests = insert_list(nb_invites, :invitation_request)
-      Accounts.send_invites(nb_invites)
-      Enum.each(requests, fn req ->
-        assert_delivered_email CaptainFactMailer.Email.invitation_to_register(req)
-      end)
-    end
-
-    test "multiple requests only insert one time but doesn't crash" do
-      email = "test@email.com"
-      user = insert(:user)
-      {:ok, req} = Accounts.request_invitation(email)
-      {:ok, req2} = Accounts.request_invitation(email)
-      {:ok, req3} = Accounts.request_invitation(email, user)
-
-      assert req.id == req2.id
-      assert req2.id == req3.id
-    end
-
-    test "cannot insert with bad email" do
-      assert {:error, "invalid_email"} == Accounts.request_invitation("toto@yopmail.fr")
-      assert {:error, "invalid_email"} == Accounts.request_invitation("toto@")
-      assert {:error, "invalid_email"} == Accounts.request_invitation("xxxxxxxxx")
-    end
-
-    test "re-asking for an invitation reset invitation_sent boolean to false" do
-      req = insert(:invitation_request, %{invitation_sent: true})
-      {:ok, req_updated} = Accounts.request_invitation(req.email)
-      assert req_updated.invitation_sent == false
-    end
-
-    # TODO What if user already have an account and request an invitation ?
   end
 
   describe "create_account" do
