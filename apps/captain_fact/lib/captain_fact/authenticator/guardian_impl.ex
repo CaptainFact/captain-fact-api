ed189f3a34ea099d6b648d293c259a5604d50ce6
Julien Leclercq <julien.leclercq@gmx.fr>
2018-07-31 18:22:38 +0200

    start homemade guardian implementation

---
+++ apps/captain_fact/lib/captain_fact/authenticator/guardian_impl.ex
@@ -0,0 +1,22 @@
+defmodule CaptainFact.Authenticator.GuardianImpl do
+  use Guardian, otp_app: :captain_fact
+
+  alias DB.Repo
+  alias DB.Schema.User
+  alias Kaur.Result
+
+  def subject_for_token(%User{id: id}, _claims) do
+    Result.ok("User:#{id}")
+  end
+
+  def subject_for_token(_,_) do
+    Result.error("token is based on a user")
+  end
+
+  def resource_from_claims(claims) do
+    "User:" <> user_id = claims["sub"]
+
+    Repo.get(%User{}, user_id)
+  end
+
+end
