3eda73b1eea54c6661ea97a4db91d97ca1268496
Julien Leclercq <julien.leclercq@gmx.fr>
2018-08-01 12:54:18 +0200

    add guardian db boilerplate to guardian implementation

--- apps/captain_fact/lib/captain_fact/authenticator/guardian_impl.ex
+++ apps/captain_fact/lib/captain_fact/authenticator/guardian_impl.ex
@@ -22,6 +22,26 @@ defmodule CaptainFact.Authenticator.GuardianImpl do
     |> Result.map_error(fn :no_value -> :user_not_found end)
   end
 
+  # ---- Guardian.DB boilerplate ----
+
+  def after_encode_and_sign(resource, claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.after_encode_and_sign(resource, claims["typ"], claims, token) do
+      {:ok, token}
+    end
+  end
+
+  def on_verify(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
+      {:ok, claims}
+    end
+  end
+
+  def on_revoke(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_revoke(claims, token) do
+      {:ok, claims}
+    end
+  end
+
   defmodule Pipeline do
     use Guardian.Plug.Pipeline,
       otp_app: :captain_fact,
