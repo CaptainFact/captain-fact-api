29e3b18e75323edb68fae5221214f4b5d2d6ab11
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-08-05 10:03:33 +0200

    Bump version to 0.8.13

--- apps/cf_utils/mix.exs
+++ apps/cf_utils/mix.exs
@@ -4,7 +4,7 @@ defmodule CF.Utils.MixProject do
   def project do
     [
       app: :cf_utils,
-      version: "0.8.12",
+      version: "0.8.13",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
