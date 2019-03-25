f6c6b87b2a9e119f7c9940921e1c6ee53c84ec34
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-03-25 22:26:25 +0100

    release: Bump version to 1.0.1

--- apps/db/mix.exs
+++ apps/db/mix.exs
@@ -4,7 +4,7 @@ defmodule DB.Mixfile do
   def project do
     [
       app: :db,
-      version: "1.0.0",
+      version: "1.0.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
