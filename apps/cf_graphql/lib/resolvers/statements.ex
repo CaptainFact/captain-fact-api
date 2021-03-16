123f9351b6d6b4c40eb41592dc4e71c10959e1b1
Victor Guennou <victor.guennou@uavia.eu>
2021-03-16 14:35:13 +0100

    Remove commented code

--- apps/cf_graphql/lib/resolvers/statements.ex
+++ apps/cf_graphql/lib/resolvers/statements.ex
@@ -19,29 +19,4 @@ defmodule CF.Graphql.Resolvers.Statements do
     |> Repo.paginate(page: offset, page_size: limit)
     |> Result.ok()
   end
-
-  # Fields
-
-  # def url(video, _, _) do
-  #   {:ok, DB.Schema.Video.build_url(video)}
-  # end
-  #
-  # def thumbnail(video, _, _) do
-  #   {:ok, DB.Schema.Video.image_url(video)}
-  # end
-  #
-  # def statements(video, _, _) do
-  #   batch({__MODULE__, :fetch_statements_by_videos_ids}, video.id, fn results ->
-  #     {:ok, Map.get(results, video.id) || []}
-  #   end)
-  # end
-  #
-  # def fetch_statements_by_videos_ids(_, videos_ids) do
-  #   Statement
-  #   |> where([s], s.video_id in ^videos_ids)
-  #   |> where([s], s.is_removed == false)
-  #   |> order_by(asc: :time)
-  #   |> Repo.all()
-  #   |> Enum.group_by(& &1.video_id)
-  # end
 end
