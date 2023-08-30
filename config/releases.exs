b90bd8c2770073b7496ab6815e4f4a457d367cc1
Benjamin Piouffle <benjamin@captainfact.io>
2023-08-30 14:04:58 +0200

    Revert "feat(postgres): add release setting for ssl (#404)"
    This reverts commit c8a039b4d9b93f0c18e55c95e67562a2ebba127b.

--- config/releases.exs
+++ config/releases.exs
@@ -73,8 +73,7 @@ config :db, DB.Repo,
   hostname: load_secret.("db_hostname"),
   username: load_secret.("db_username"),
   password: load_secret.("db_password"),
-  database: load_secret.("db_name"),
-  ssl: load_bool.({"db_ssl", "false"})
+  database: load_secret.("db_name")
 
 config :ex_aws,
   access_key_id: [load_secret.("s3_access_key_id"), :instance_role],
