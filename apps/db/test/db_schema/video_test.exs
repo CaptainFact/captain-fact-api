bbdb30b06e5df9f5002699602bfa3a39da1f1b0b
Benjamin Piouffle <benjamin@captainfact.io>
2024-11-09 10:40:58 +0100

    fix: Increate max video length + truncate

--- apps/db/test/db_schema/video_test.exs
+++ apps/db/test/db_schema/video_test.exs
@@ -34,14 +34,10 @@ defmodule DB.Schema.VideoTest do
       refute changeset.valid?
     end
 
-    test "title must be between 5 and 120 characters" do
+    test "title must be between 5 and 130 characters" do
       # Too short
-      attrs = %{title: "x"}
+      attrs = %{title: "x", url: "https://www.youtube.com/watch?v=zoP-XFuWstw"}
       assert {:title, "should be at least 5 character(s)"} in errors_on(%Video{}, attrs)
-
-      # Too long
-      attrs = %{title: String.duplicate("x", 121)}
-      assert {:title, "should be at most 120 character(s)"} in errors_on(%Video{}, attrs)
     end
 
     test "unknown or invalid language gives nil values but aren't rejected" do
